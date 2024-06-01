bb851a89e5ae5679dfa230db3c1c99045b193a74
najia01 <validress@outlook.fr>
2024-06-01 21:50:39 +0200

    fonction cart done

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,8 +2,97 @@
 
 namespace App\Classe;
 
-class Cart{
-public function add(){
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Cart
+{
+    public function __construct(private RequestStack $requestStack)
+    {
+    }
+
+    public function add($product)
+    {
+
+        // on appelle la session de symfony
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+
+        // On ajoute une qté +1
+        // Si mon produit est déjà dans mon panier
+        if (isset($cart[$product->getId()])) {
+            $cart[$product->getId()] = [
+                'object' => $product,
+                'qty' => $cart[$product->getId()]['qty'] + 1
+            ];
+        } else {
+            $cart[$product->getId()] = [
+                'object' => $product,
+                'qty' => 1
+            ];
+        }
+
+        // On crée la session cart
+        $session->set('cart', $cart);
+    }
     
-}
+    public function remove()
+    {
+       return $this->requestStack->getSession()->remove('cart');
+    }
+
+    public function getCart()
+    {
+        return $this->requestStack->getSession()->get('cart');
+    }
+
+    public function decrease($id)
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+
+        if ($cart[$id]['qty'] > 1) {
+            $cart[$id]['qty'] = $cart[$id]['qty']- 1;
+
+        } else {
+
+           unset($cart[$id]);
+        }
+
+        $session->set('cart', $cart);
+        
+    }
+
+    public function fullQuantity()
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+        $quantity = 0;
+
+        if (!isset ($cart) ){
+            return $quantity;
+        }
+
+        foreach($cart as $product){
+            $quantity = $quantity + $product['qty'];
+        }
+        return $quantity;
+    }
+
+    public function getTotalWt()
+    {
+        $session = $this->requestStack->getSession();
+        $cart = $session->get('cart', []);
+        $price = 0;
+
+        if (!isset ($cart) ){
+            return $price;
+        }
+
+
+        foreach($cart as $product){
+            $price = $price + ($product ['object']->getPriceWt()* $product['qty']);
+        }
+        return $price;
+    }
+   
 }
