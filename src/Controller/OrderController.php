0fb2d68e5d3603d0bf7116ed31c4676bf87d33f8
najia01 <validress@outlook.fr>
2024-06-06 23:13:33 +0200

    including stripe

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -88,6 +88,7 @@ class OrderController extends AbstractController
         return $this->render('order/summary.html.twig', [
             'choices'=> $form->getData(),
             'cart'=> $products ,
+            'order'=> $order ,
             'totalWt'=>$cart->getTotalWt(),
         ]);
     }
