96b303070c0abd307a3d00972e9405a43d891327
najia01 <validress@outlook.fr>
2024-06-09 23:17:26 +0200

    add mailjet

---
+++ src/Classe/Mail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Classe;
+
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class Mail 
+{
+    public function send($to_email, $to_name, $subject, $content)
+    {
+        $mj = new Client($_ENV['MJ_APIKEY_PUBLIC'], $_ENV['MJ_APIKEY_PRIVATE'],true,['version' => 'v3.1']);
+      
+
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "macocotte01250@gmail.com",
+                        'Name' => "La boutique française"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $to_email,
+                            'Name' => $to_name
+                        ]
+                    ],
+                    'Subject' => $subject,
+                    'TextPart' => "Greetings from Mailjet!",
+                    'HTMLPart' => $content
+                ]
+            ]
+        ];
+
+        $mj->post(Resources::$Email, ['body' => $body]);
+
+    }
+
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Mail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,6 +12,10 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        
+       $mail = new Mail();
+       $mail->send('laboutiquefrench@yopmail.com','Gina Doe','Bonjour test de ma classe mail', 'mon premier email' );
+
         return $this->render('home/index.html.twig');
     }
 }
